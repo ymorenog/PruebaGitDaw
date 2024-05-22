306e0a2d1490376123169c891fa6e6177594ce43
ymorenog <yolandamorenog@outlook.com>
2024-05-22 08:53:27 +0200

    añado mensaje adios rama

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -4,6 +4,7 @@ public class ClasePrueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola");
+		System.out.println("adios rama");
 	}
 
 }
